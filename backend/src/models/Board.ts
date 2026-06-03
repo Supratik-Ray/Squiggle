619cde6df522541d61e17d4c3f234823a90f6085
Supratik <raysupratik8@gmail.com>
2026-06-03 17:15:02 +0530

    added Board mongoose Schema

---
+++ backend/src/models/Board.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const boardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trime: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    roomId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    ownerId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    canvasData: {
+      type: Schema.Types.Mixed,
+      default: {},
+    },
+  },
+  { timestamps: true },
+);
+
+export const Board = mongoose.model("Board", boardSchema);
